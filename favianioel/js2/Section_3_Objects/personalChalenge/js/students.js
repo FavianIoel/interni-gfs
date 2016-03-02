98d9b06980ebe37b8938433b3daf01085039163e
Favianioel <favianioel@gmail.com>
2016-03-02 21:51:52 +0200

    ++

---
+++ favianioel/js2/Section_3_Objects/personalChalenge/js/students.js
@@ -0,0 +1,32 @@
+var studenti = [ 
+  { 
+    nume: 'Favian',
+    track: 'PHP Development',
+    realizari: 158,
+    puncte: 14730
+  },
+  {
+    nume: 'Teo',
+    track: 'Java Developer',
+    realizari: '175',
+    puncte: '16375'
+  },
+  {
+    nume: 'Romina',
+    track: 'Front end Development',
+    realizari: '55',
+    puncte: '2025'
+  },
+  {
+    nume: 'Andrei',
+    track: 'C## Development',
+    realizari: '40',
+    puncte: '1950'
+  },
+  {
+    nume: 'Maria',
+    track: 'Back end Development',
+    realizari: '5',
+    puncte: '350'
+  }
+];
