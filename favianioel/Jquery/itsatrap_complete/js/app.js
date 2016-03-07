c8b804e75ad3fe44321bf439d97ca7777865a1d8
Favian Ioel Poputa <favianioel@gmail.com>
2016-03-07 12:26:54 +0200

    more lines od jquery

--- favianioel/Jquery/itsatrap_complete/js/app.js
+++ favianioel/Jquery/itsatrap_complete/js/app.js
@@ -1,3 +1,13 @@
-//Hide Warning
-//Show Warning Slowly
-$(".warning").hide().show("slower");
+//Hide Warning & Show Warning Slowly
+//create de function
+
+function mycode () {
+    $(".warning").hide().show("slow");
+}
+
+//All three of the following syntaxes are equivalent:
+
+    //$( document ).ready( handler )
+    //$( handler )
+
+$(document).ready(mycode);
