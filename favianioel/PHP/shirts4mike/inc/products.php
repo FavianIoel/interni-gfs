f30203022dce9b3f8856b498ba82cdc90b55c631
Favian Ioel <favianioel@gmail.com>
2016-03-24 17:42:06 +0200

    paypal2 day

---
+++ favianioel/PHP/shirts4mike/inc/products.php
@@ -0,0 +1,51 @@
+<?php 
+$products[101] = array(
+	"name" => "Logo Shirt, Red",
+	"img" => "img/shirts/shirt-101.jpg",
+	"price" => 20,
+	"paypal" => "CS823L78DMD4G"
+	);
+$products[102] = array(
+	"name" => "Mike frog Shirt, Gray",
+	"img" => "img/shirts/shirt-102.jpg",
+	"price" => 25,
+	"paypal" => "2M9Z3YLSGZT32"
+	);
+$products[103] = array(
+	"name" => "Mike frog Shirt, Blue",
+	"img" => "img/shirts/shirt-103.jpg",
+	"price" => 25,
+	"paypal" => "7MYSRYXMGHGR8"
+	);
+$products[104] = array(
+	"name" => "Logo Shirt, Green",
+	"img" => "img/shirts/shirt-104.jpg",
+	"price" => 20,
+	"paypal" => "HVHMWJRJKCDQU"
+	);
+$products[105] = array(
+	"name" => "Mike frog Shirt, Yellow",
+	"img" => "img/shirts/shirt-105.jpg",
+	"price" => 25,
+	"paypal" => "URX8CH92WMHCG"
+	);
+$products[106] = array(
+	"name" => "Logo Shirt, Gray",
+	"img" => "img/shirts/shirt-106.jpg",
+	"price" => 20,
+	"paypal" => "A6WBQL8B6J9N8"
+	);
+$products[107] = array(
+	"name" => "Logo Shirt, Turquoise",
+	"img" => "img/shirts/shirt-107.jpg",
+	"price" => 20,
+	"paypal" => "SRH36K3HFF9RL"
+	);
+$products[108] = array(
+	"name" => "Mike frog Shirt, Orange",
+	"img" => "img/shirts/shirt-108.jpg",
+	"price" => 25,
+	"paypal" => "5HSUKN6MH52YU"
+	);
+
+	?>
